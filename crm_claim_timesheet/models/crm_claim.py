84f1055a136caa600eb25a7e4b30bdddf21c907f
Oihane Crucelaegui <oihanecruce@gmail.com>
2021-04-22 11:27:52 +0200

    [IMP] crm_claim_timesheet: view hours like time

--- crm_claim_timesheet/models/crm_claim.py
+++ crm_claim_timesheet/models/crm_claim.py
@@ -40,6 +40,11 @@ class CrmClaim(models.Model):
         compute_sudo=True,
         store=True,
         help="Time spent on this claim.")
+    task_remaining_hours = fields.Float(
+        string="Task Remaining Hours",
+        related="task_id.remaining_hours",
+        readonly=True,
+    )
 
     @api.depends('timesheet_ids.unit_amount')
     def _compute_effective_hours(self):
@@ -51,3 +56,49 @@ class CrmClaim(models.Model):
     def _compute_remaining_hours(self):
         for claim in self:
             claim.remaining_hours = claim.planned_hours - claim.effective_hours
+
+    @api.onchange("partner_id")
+    def onchange_partner_id(self):
+        super().onchange_partner_id()
+        projects = self.env["project.project"].search([
+            ("partner_id", "=", self.commercial_partner_id.id)])
+        if len(projects) == 1:
+            self.project_id = projects[:1]
+
+    @api.onchange("project_id")
+    def _onchange_project_id(self):
+        self.task_id = (
+            self.project_id.task_ids[:1] if len(
+                self.project_id.task_ids) == 1 else False)
+
+    @api.model
+    def message_new(self, msg, custom_values=None):
+        """Overrides mail_thread message_new that is called by the mailgateway
+        through message_process.
+        This override updates the document according to the email.
+        """
+        if custom_values is None:
+            custom_values = {}
+        partner_id = msg.get("author_id", False)
+        defaults = {}
+        if partner_id:
+            partner = self.env["res.partner"].browse(partner_id)
+            tasks = self.env["project.task"].search([
+                ("project_id.partner_id", "=",
+                 partner.commercial_partner_id.id),
+                "|", ("stage_id", "=", False),
+                ("stage_id.is_closed", "=", False)
+            ])
+            if len(tasks) == 1:
+                defaults.update({
+                    "task_id": tasks[:1].id,
+                    "project_id": tasks[:1].project_id.id,
+                })
+            else:
+                projects = tasks.mapped("project_id")
+                if len(projects) == 1:
+                    defaults.update({
+                        "project_id": projects[:1].id,
+                    })
+        defaults.update(custom_values)
+        return super(CrmClaim, self).message_new(msg, custom_values=defaults)
