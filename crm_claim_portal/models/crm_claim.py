f419046660ab282a3e2d60296b0a2c77bb00427c
Oihane Crucelaegui <oihanecruce@gmail.com>
2021-07-08 11:57:28 +0200

    [FIX] crm_claim_portal: mail links failing for portal users

--- crm_claim_portal/models/crm_claim.py
+++ crm_claim_portal/models/crm_claim.py
@@ -7,3 +7,8 @@ from odoo import models
 class CrmClaim(models.Model):
     _name = "crm.claim"
     _inherit = ["crm.claim", "portal.mixin", "rating.mixin"]
+
+    def _compute_access_url(self):
+        super(CrmClaim, self)._compute_access_url()
+        for task in self:
+            task.access_url = '/my/claim/%s' % task.id
