2af581bd633dcda981d890676cbd1e32c4e33d1d
Oihane Crucelaegui <oihanecruce@gmail.com>
2021-07-16 21:16:15 +0200

    [ADD] New module crm_claim_close

---
+++ crm_claim_close/models/crm_claim_stage.py
@@ -0,0 +1,10 @@
+# Copyright 2021 Oihane Crucelaegui - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class CrmClaimStage(models.Model):
+    _inherit = "crm.claim.stage"
+
+    closed = fields.Boolean(string="Closed")
