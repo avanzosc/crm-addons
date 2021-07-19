cfd4f7fc2ff348cbf09cfb40764b455b35d0e5a1
oihane <oihanecruce@gmail.com>
2021-07-19 14:57:25 +0200

    [ADD] New module crm_claim_close (#73)

---
+++ crm_claim_close/tests/common.py
@@ -0,0 +1,19 @@
+# Copyright 2021 Oihane Crucelaegui - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo.tests import common
+
+
+class CrmClaimCommon(common.SavepointCase):
+    @classmethod
+    def setUpClass(cls):
+        super(CrmClaimCommon, cls).setUpClass()
+
+        cls.claim_model = cls.env["crm.claim"].with_context(
+            mail_create_nosubscribe=True)
+        claim_stage_model = cls.env["crm.claim.stage"]
+
+        cls.open_stage = claim_stage_model.search(
+            [("closed", "=", False)], limit=1)
+        cls.close_stage = claim_stage_model.search(
+            [("closed", "=", True)], limit=1)
