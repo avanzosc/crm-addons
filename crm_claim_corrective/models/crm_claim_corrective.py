0d48de22b388601854915b5b2cbfa3f94883aaba
Daniel <7643575+Daniel-CA@users.noreply.github.com>
2021-07-28 11:48:18 +0200

    [ADD][14.0] crm_claim_corrective (#74)

---
+++ crm_claim_corrective/models/crm_claim_corrective.py
@@ -0,0 +1,48 @@
+# Copyright 2021 Daniel Campos - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, fields, models
+
+
+class CrmClaimCorrective(models.Model):
+    _name = 'crm.claim.corrective'
+
+    name = fields.Char(string='Sequence', default="/", required=True)
+    claim_id = fields.Many2one(comodel_name='crm.claim', string='Claim',
+                               help='Select a Claim', readonly=True)
+    partner_id = fields.Many2one(
+        comodel_name='res.partner', related="claim_id.partner_id",
+        string="Customer", store=True, readonly=True)
+    corrective_action_ids = fields.One2many(
+        comodel_name="crm.claim.corrective.action",
+        inverse_name="corrective_id", string="Corrective actions")
+    state = fields.Selection([('draft', 'Draft'),
+                              ('pending', 'Pending'),
+                              ('closed', 'Closed')], string="State",
+                             default='draft', copy=False)
+
+    @api.model
+    def create(self, vals):
+        if vals.get("name", "/") == "/":
+            vals["name"] = self.env.ref(
+                "crm_claim_corrective.seq_corrective_action",
+                raise_if_not_found=False).next_by_id()
+        return super().create(vals)
+
+
+class CrmClaimCorrectiveAction(models.Model):
+    _name = 'crm.claim.corrective.action'
+    _order = "sequence"
+
+    corrective_id = fields.Many2one(comodel_name='crm.claim.corrective',
+                                    string='Corrective info')
+    name = fields.Char(string='Name', required=True)
+    sequence = fields.Integer(string='Sequence')
+    claim_id = fields.Many2one(
+        comodel_name='crm.claim', related="corrective_id.claim_id",
+        string="Claim", store=True, readonly=True)
+    responsible_id = fields.Many2one(comodel_name='res.users',
+                                     string='Responsible',
+                                     help='Select a Responsible')
+    date_planned = fields.Date(string='Planned Date')
+    date_done = fields.Date(string='Date Done')
