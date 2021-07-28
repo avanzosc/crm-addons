2cc98254bb41cd571cec7e274ecf30e3ded16673
Daniel <7643575+Daniel-CA@users.noreply.github.com>
2021-07-28 11:48:18 +0200

    [ADD][14.0] crm_claim_corrective (#74)

---
+++ crm_claim_corrective/models/__init__.py
@@ -0,0 +1,3 @@
+
+from . import crm_claim
+from . import crm_claim_corrective
---
+++ crm_claim_corrective/tests/test_crm_claim_corrective.py
@@ -0,0 +1,30 @@
+# Copyright 2021 Daniel Campos - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from .common import CrmClaimCorrectiveCommon
+from odoo.tests import tagged
+
+
+@tagged("post_install", "-at_install")
+class TestACrmClaimCorrective(CrmClaimCorrectiveCommon):
+
+    def test_crm_claim_corrective_name(self):
+        new_name = self._get_next_name()
+        corrective = self.corrective_model.create({"name": "/"})
+        self.assertEqual(corrective.name, new_name)
+
+    def test_crm_claim_onchange(self):
+        self.claim.claim_description_id = self.claim_descrp.id
+        self.claim.onchange_claim_description()
+        self.assertEqual(self.claim.description, self.claim_descrp.description)
+        self.claim.cause_description_id = self.cause_descrp.id
+        self.claim.onchange_cause_description()
+        self.assertEqual(self.claim.cause, self.cause_descrp.description)
+        self.claim.resolution_description_id = self.resolution_descrp.id
+        self.claim.onchange_resolution_description()
+        self.assertEqual(self.claim.resolution,
+                         self.resolution_descrp.description)
+
+    def _get_next_name(self):
+        return self.corrective_sequence.get_next_char(
+            self.corrective_sequence.number_next_actual)
