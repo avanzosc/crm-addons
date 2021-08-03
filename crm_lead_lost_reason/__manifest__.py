414766c07036c08d458964bb12bd37da117070da
Thor <alfredodelafuente@avanzosc.es>
2021-08-03 13:56:43 +0200

    [ADD] crm_lead_lost_reason: Show button "Lost reason" in leads.

---
+++ crm_lead_lost_reason/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2021 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Crm Lead Lost Reason",
+    "version": "14.0.1.0.0",
+    "category": "Customer Relationship Management",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "crm",
+    ],
+    "data": [
+        "views/crm_lead_views.xml",
+    ],
+    "installable": True,
+}
