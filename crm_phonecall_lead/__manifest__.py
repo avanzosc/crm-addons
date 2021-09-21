29eca5ca4847f556ec3345dbc4f69d9e12e438b9
Tu Nombre <you@example.com>
2021-09-21 12:31:05 +0200

    [ADD] crm_phonecall_lead: Convert to lead or opportunity button in phone call instead of only convert to opportunity.

---
+++ crm_phonecall_lead/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2021 Berezi - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "CRM Phonecall Lead",
+    'version': '14.0.1.0.0',
+    "author": "Avanzosc",
+    "category": "Sales/CRM",
+    "depends": [
+        "crm_phonecall",
+        "crm"
+    ],
+    "data": [
+        "views/crm_phonecall_views.xml",
+    ],
+    "license": "AGPL-3",
+    'installable': True,
+}
