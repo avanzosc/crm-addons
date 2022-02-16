f92998fbdfbe45ffa5f830da3ead1d7b5f77ee97
Oihane Crucelaegui <oihanecruce@gmail.com>
2022-02-16 10:11:40 +0100

    [IMP] crm_claim_close: add new filters

---
+++ crm_claim_usability/models/res_config_settings.py
@@ -0,0 +1,12 @@
+# Copyright 2021 Oihane Crucelaegui - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = "res.config.settings"
+
+    group_crm_claim_technical = fields.Boolean(
+        string="Show Metadata Info in Claims",
+        implied_group="crm_claim_usability.group_crm_claim_technical")
