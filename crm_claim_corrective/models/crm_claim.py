0cfaf3c5f5cd2cb3d16fe14323abdcab841dc551
Oihane Crucelaegui <oihanecruce@gmail.com>
2022-03-02 11:38:01 +0100

    [IMP] crm_claim_corrective: create corrective action using a button

--- crm_claim_corrective/models/crm_claim.py
+++ crm_claim_corrective/models/crm_claim.py
@@ -7,21 +7,25 @@ from odoo import api, fields, models
 class CrmClaim(models.Model):
     _inherit = "crm.claim"
 
-    photo1 = fields.Binary(string='Photo1', attachment=True)
-    photo2 = fields.Binary(string='Photo2', attachment=True)
-    photo3 = fields.Binary(string='Photo3', attachment=True)
-    photo4 = fields.Binary(string='Photo4', attachment=True)
+    photo1 = fields.Binary(string="Photo1", attachment=True)
+    photo2 = fields.Binary(string="Photo2", attachment=True)
+    photo3 = fields.Binary(string="Photo3", attachment=True)
+    photo4 = fields.Binary(string="Photo4", attachment=True)
     claim_description_id = fields.Many2one(
-        comodel_name="crm.claim.description", string="Claim Description",
-        domain="[('type_description','=', 'claim')]")
+        comodel_name="crm.claim.description",
+        string="Claim Description",
+        domain="[('type_description', '=', 'claim')]")
     cause_description_id = fields.Many2one(
-        comodel_name="crm.claim.description", string="Cause Description",
-        domain="[('type_description','=', 'cause')]")
+        comodel_name="crm.claim.description",
+        string="Cause Description",
+        domain="[('type_description', '=', 'cause')]")
     resolution_description_id = fields.Many2one(
-        comodel_name="crm.claim.description", string="Resolution Description",
+        comodel_name="crm.claim.description",
+        string="Resolution Description",
         domain="[('type_description','=', 'resolution')]")
     corrective_id = fields.Many2one(
-        comodel_name="crm.claim.corrective",  string="Claim corrective")
+        comodel_name="crm.claim.corrective",
+        string="Claim Corrective")
 
     @api.onchange("claim_description_id")
     def onchange_claim_description(self):
@@ -43,12 +47,21 @@ class CrmClaim(models.Model):
         if self.corrective_id:
             self.corrective_id.claim_id = self._origin.id
 
+    def button_create_corrective(self):
+        corrective_obj = self.env["crm.claim.corrective"]
+        for claim in self.filtered(lambda r: not r.corrective_id):
+            corrective = corrective_obj.create({
+                "claim_id": claim.id,
+            })
+            claim.corrective_id = corrective
+
 
 class CrmClaimDescription(models.Model):
-    _name = 'crm.claim.description'
+    _name = "crm.claim.description"
+    _description = "Claim Description"
 
-    name = fields.Char(string='Name', required=True)
-    description = fields.Text(string='Description')
+    name = fields.Char(string="Name", required=True)
+    description = fields.Text(string="Description")
     type_description = fields.Selection(
         selection=[
             ("claim", "Claim Description"),
@@ -57,3 +70,4 @@ class CrmClaimDescription(models.Model):
         ],
         string="Description Type",
     )
+
