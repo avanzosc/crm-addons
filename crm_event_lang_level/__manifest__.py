1fcd07f50243ddc3c0af94e563654e4dc9fac533
Thor <alfredodelafuente@avanzosc.es>
2022-05-20 09:28:19 +0200

    [ADD] crm_event_lang_level: New fields language and level in "Lead/Opportunity".

---
+++ crm_event_lang_level/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2022 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "CRM Event Lang Level",
+    'version': '14.0.1.0.0',
+    "author": "Avanzosc",
+    "website": "https://github.com/avanzosc/crm-addons.git",
+    "category": "Customer Relationship Management",
+    "depends": [
+        "crm",
+        "event_lang_level",
+    ],
+    "data": [
+        "views/crm_lead_views.xml",
+    ],
+    "license": "AGPL-3",
+    'installable': True,
+}
---
+++ crm_event_lang_level/models/crm_lead.py
@@ -0,0 +1,12 @@
+# Copyright 2022 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import models, fields, api
+
+
+class CrmLead(models.Model):
+    _inherit = 'crm.lead'
+
+    lang_id = fields.Many2one(
+        string='Language', comodel_name='hr.skill')
+    level_id = fields.Many2one(
+        string='Level', comodel_name='hr.skill.level')
