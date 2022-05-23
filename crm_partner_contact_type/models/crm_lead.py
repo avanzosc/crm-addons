d8aefbad976b6edb02edc7f9adf95f4bd80e3787
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2022-05-23 11:31:11 +0200

    [ADD] crm_partner_contact_type: New field "Contact type" in "Lead/Opportunity". (#108)

---
+++ crm_partner_contact_type/models/crm_lead.py
@@ -0,0 +1,10 @@
+# Copyright 2022 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import models, fields, api
+
+
+class CrmLead(models.Model):
+    _inherit = 'crm.lead'
+
+    contact_type_id = fields.Many2one(
+        string='Contact type', comodel_name='res.partner.type')
