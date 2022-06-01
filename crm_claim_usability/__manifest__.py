f79758ee9d4dd65a9eb664eda2dab10cf6e74013
Oihane Crucelaegui <oihanecruce@gmail.com>
2022-06-01 14:50:07 +0200

    [MIG] crm_claim_usability: Migration to 15.0

--- crm_claim_usability/__manifest__.py
+++ crm_claim_usability/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Claims Usability",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "category": "Customer Relationship Management",
     "license": "AGPL-3",
     "author": "AvanzOSC",
