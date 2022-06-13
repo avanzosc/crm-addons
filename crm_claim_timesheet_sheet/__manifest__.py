ee14750541c10bb83deb5f03559567542e722292
Oihane Crucelaegui <oihanecruce@gmail.com>
2022-06-13 16:19:30 +0200

    [ADD] New module crm_claim_timesheet_sheet

---
+++ crm_claim_timesheet_sheet/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2022 Oihane Crucelaegui - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Claim Timesheet Sheet",
+    "version": "14.0.1.0.0",
+    "category": "Human Resources",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/crm-addons",
+    "depends": [
+        "crm_claim_timesheet",
+        "hr_timesheet_sheet",
+    ],
+    "data": [
+        "views/hr_timesheet_sheet_views.xml",
+    ],
+    "installable": True,
+    "auto_install": True,
+}
