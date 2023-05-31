8f08270df3a57045a6e3aaf8655ae92d9abcd7d3
Oihane Crucelaegui <oihanecruce@gmail.com>
2023-05-31 10:13:27 +0200

    [IMP] crm_claim_timesheet: pre-commit stuff

--- crm_claim_timesheet/models/crm_claim.py
+++ crm_claim_timesheet/models/crm_claim.py
@@ -33,26 +33,29 @@ class CrmClaim(models.Model):
         store=True,
         readonly=True,
         help="Total remaining time, can be re-estimated periodically by the "
-             "assignee of the claim.")
+        "assignee of the claim.",
+    )
     effective_hours = fields.Float(
         string="Hours Spent",
         compute="_compute_effective_hours",
         compute_sudo=True,
         store=True,
-        help="Time spent on this claim.")
+        help="Time spent on this claim.",
+    )
     task_remaining_hours = fields.Float(
         string="Task Remaining Hours",
         related="task_id.remaining_hours",
         readonly=True,
     )
 
-    @api.depends('timesheet_ids.unit_amount')
+    @api.depends("timesheet_ids.unit_amount")
     def _compute_effective_hours(self):
         for claim in self:
             claim.effective_hours = round(
-                sum(claim.timesheet_ids.mapped('unit_amount')), 2)
+                sum(claim.timesheet_ids.mapped("unit_amount")), 2
+            )
 
-    @api.depends('effective_hours', 'planned_hours')
+    @api.depends("effective_hours", "planned_hours")
     def _compute_remaining_hours(self):
         for claim in self:
             claim.remaining_hours = claim.planned_hours - claim.effective_hours
@@ -60,16 +63,19 @@ class CrmClaim(models.Model):
     @api.onchange("partner_id")
     def onchange_partner_id(self):
         super().onchange_partner_id()
-        projects = self.env["project.project"].search([
-            ("partner_id", "=", self.commercial_partner_id.id)])
+        projects = self.env["project.project"].search(
+            [("partner_id", "=", self.commercial_partner_id.id)]
+        )
         if len(projects) == 1:
             self.project_id = projects[:1]
 
     @api.onchange("project_id")
     def _onchange_project_id(self):
         self.task_id = (
-            self.project_id.task_ids[:1] if len(
-                self.project_id.task_ids) == 1 else False)
+            self.project_id.task_ids[:1]
+            if len(self.project_id.task_ids) == 1
+            else False
+        )
 
     @api.model
     def message_new(self, msg, custom_values=None):
@@ -83,22 +89,28 @@ class CrmClaim(models.Model):
         defaults = {}
         if partner_id:
             partner = self.env["res.partner"].browse(partner_id)
-            tasks = self.env["project.task"].search([
-                ("project_id.partner_id", "=",
-                 partner.commercial_partner_id.id),
-                "|", ("stage_id", "=", False),
-                ("stage_id.is_closed", "=", False)
-            ])
+            tasks = self.env["project.task"].search(
+                [
+                    ("project_id.partner_id", "=", partner.commercial_partner_id.id),
+                    "|",
+                    ("stage_id", "=", False),
+                    ("stage_id.is_closed", "=", False),
+                ]
+            )
             if len(tasks) == 1:
-                defaults.update({
-                    "task_id": tasks[:1].id,
-                    "project_id": tasks[:1].project_id.id,
-                })
+                defaults.update(
+                    {
+                        "task_id": tasks[:1].id,
+                        "project_id": tasks[:1].project_id.id,
+                    }
+                )
             else:
                 projects = tasks.mapped("project_id")
                 if len(projects) == 1:
-                    defaults.update({
-                        "project_id": projects[:1].id,
-                    })
+                    defaults.update(
+                        {
+                            "project_id": projects[:1].id,
+                        }
+                    )
         defaults.update(custom_values)
         return super(CrmClaim, self).message_new(msg, custom_values=defaults)
