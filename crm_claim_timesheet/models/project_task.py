cdc8af62e0e7969555ecf4ecfbaa21c441197599
Oihane Crucelaegui <oihanecruce@gmail.com>
2023-05-31 10:13:27 +0200

    [IMP] crm_claim_timesheet: pre-commit stuff

--- crm_claim_timesheet/models/project_task.py
+++ crm_claim_timesheet/models/project_task.py
@@ -7,13 +7,10 @@ from odoo import api, fields, models
 class ProjectTask(models.Model):
     _inherit = "project.task"
 
-    claim_count = fields.Integer(
-        string="# Claims",
-        compute="_compute_claim_count")
+    claim_count = fields.Integer(string="# Claims", compute="_compute_claim_count")
     claim_ids = fields.One2many(
-        comodel_name="crm.claim",
-        inverse_name="task_id",
-        string="Claims")
+        comodel_name="crm.claim", inverse_name="task_id", string="Claims"
+    )
 
     @api.depends("claim_ids")
     def _compute_claim_count(self):
