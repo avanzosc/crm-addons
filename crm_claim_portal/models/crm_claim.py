17e34a71d43260a19c7d03e0eb62b0c2b219d345
Oihane Crucelaegui <oihanecruce@gmail.com>
2023-08-10 16:11:33 +0200

    [IMP] crm_claim_portal: allow creating claims from portal

--- crm_claim_portal/models/crm_claim.py
+++ crm_claim_portal/models/crm_claim.py
@@ -6,10 +6,10 @@ from odoo import models
 
 class CrmClaim(models.Model):
     _name = "crm.claim"
-    _inherit = ["crm.claim", "portal.mixin", "rating.mixin"]
+    _inherit = ["crm.claim", "portal.mixin"]
 
     def _compute_access_url(self):
-        super(CrmClaim, self)._compute_access_url()
+        super()._compute_access_url()
         for task in self:
             task.access_url = "/my/claim/%s" % task.id
 
