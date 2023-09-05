67b70232007f84c775c8868781e63514cd1974af
Oihane Crucelaegui <oihanecruce@gmail.com>
2023-09-05 11:41:25 +0200

    [MIG] crm_claim_timesheet_sheet: Migration to 16.0

--- crm_claim_timesheet_sheet/__manifest__.py
+++ crm_claim_timesheet_sheet/__manifest__.py
@@ -3,8 +3,8 @@
 
 {
     "name": "Claim Timesheet Sheet",
-    "version": "14.0.1.0.0",
-    "category": "Human Resources",
+    "version": "16.0.1.0.0",
+    "category": "Customer Relationship Management",
     "license": "AGPL-3",
     "author": "AvanzOSC",
     "website": "https://github.com/avanzosc/crm-addons",
