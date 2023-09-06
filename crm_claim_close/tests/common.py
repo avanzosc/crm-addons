1c313070fc104fb8b202f313509c09792163051b
Oihane Crucelaegui <oihanecruce@gmail.com>
2023-09-06 10:44:23 +0200

    [IMP] crm_claim_close: pre-commit stuff

--- crm_claim_close/tests/common.py
+++ crm_claim_close/tests/common.py
@@ -7,13 +7,12 @@ from odoo.tests import common
 class CrmClaimCommon(common.SavepointCase):
     @classmethod
     def setUpClass(cls):
-        super(CrmClaimCommon, cls).setUpClass()
+        super().setUpClass()
 
         cls.claim_model = cls.env["crm.claim"].with_context(
-            mail_create_nosubscribe=True)
+            mail_create_nosubscribe=True
+        )
         claim_stage_model = cls.env["crm.claim.stage"]
 
-        cls.open_stage = claim_stage_model.search(
-            [("closed", "=", False)], limit=1)
-        cls.close_stage = claim_stage_model.search(
-            [("closed", "=", True)], limit=1)
+        cls.open_stage = claim_stage_model.search([("closed", "=", False)], limit=1)
+        cls.close_stage = claim_stage_model.search([("closed", "=", True)], limit=1)
