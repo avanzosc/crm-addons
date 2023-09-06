34b80c87d625df98b3baff213d3f49e8faac2c23
Oihane Crucelaegui <oihanecruce@gmail.com>
2023-09-06 10:44:23 +0200

    [IMP] crm_claim_close: pre-commit stuff

--- crm_claim_close/hooks.py
+++ crm_claim_close/hooks.py
@@ -1,9 +1,10 @@
 # Copyright 2021 Oihane Crucelaegui - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
+import logging
+
 from odoo import SUPERUSER_ID, api
 
-import logging
 _logger = logging.getLogger(__name__)
 
 
