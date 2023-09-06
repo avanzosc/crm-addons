b55b6cbf4f137ecf95966899af92db63a014a6d1
Oihane Crucelaegui <oihanecruce@gmail.com>
2023-09-06 11:37:01 +0200

    [MIG] crm_claim_portal: Migration to 16.0

--- crm_claim_portal/__manifest__.py
+++ crm_claim_portal/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "CRM Claim Portal",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Customer Relationship Management",
     "license": "AGPL-3",
     "author": "AvanzOSC",
