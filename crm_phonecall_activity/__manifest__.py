2f2920862773febc946d4841f1a6225258345392
Unai Beristain <unaiberistain@avanzosc.es>
2023-12-20 15:45:24 +0100

    [IMP] crm_phonecall_activity: pre-commit stuff

--- crm_phonecall_activity/__manifest__.py
+++ crm_phonecall_activity/__manifest__.py
@@ -2,16 +2,13 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "CRM Phonecall Activity",
-    'version': '14.0.1.0.0',
+    "version": "14.0.1.0.0",
     "author": "Avanzosc",
     "category": "Sales/CRM",
-    "depends": [
-        "crm_phonecall",
-        "mail"
-    ],
+    "depends": ["crm_phonecall", "mail"],
     "data": [
         "views/crm_phonecall_views.xml",
     ],
     "license": "AGPL-3",
-    'installable': True,
+    "installable": True,
 }
