138525eefaaa984de96e58ae54cf6c1c342bb27d
Unai Beristain <unaiberistain@avanzosc.es>
2023-12-27 10:48:59 +0100

    [IMP] crm_claim_deadline: pre-commit stuff

--- crm_claim_deadline/models/res_company.py
+++ crm_claim_deadline/models/res_company.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 # (c) 2015 Alfredo de la Fuente - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from openerp import models, fields
+from openerp import fields, models
 
 
 class ResCompany(models.Model):
-    _inherit = 'res.company'
+    _inherit = "res.company"
 
-    claim_closing_days = fields.Integer(string='Claim closing days')
+    claim_closing_days = fields.Integer(string="Claim closing days")
