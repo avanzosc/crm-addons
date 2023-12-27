43923a1b5e9ee87da4ffdfc372178f8d39beaa56
Unai Beristain <unaiberistain@avanzosc.es>
2023-12-27 10:49:06 +0100

    [MIG] crm_claim_deadline: Migration to 16.0

--- crm_claim_deadline/models/crm_claim.py
+++ crm_claim_deadline/models/crm_claim.py
@@ -1,13 +1,12 @@
 # (c) 2015 Alfredo de la Fuente - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 from dateutil.relativedelta import relativedelta
-from openerp import api, fields, models
+from odoo import api, fields, models
 
 
 class CrmClaim(models.Model):
     _inherit = "crm.claim"
 
-    @api.multi
     def _get_date_deadline(self):
         if self.env.user.company_id.claim_closing_days:
             return fields.Date.to_string(
