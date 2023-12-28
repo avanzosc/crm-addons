56ce4b450c8a4e22ef96849aebb9f74d76488112
Unai Beristain <unaiberistain@avanzosc.es>
2023-12-28 11:08:43 +0100

    [IMP] crm_claim_related: pre-commit stuff

--- crm_claim_related/__openerp__.py
+++ crm_claim_related/__openerp__.py
@@ -1,22 +1,21 @@
-# -*- coding: utf-8 -*-
 # (c) 2016 Alfredo de la Fuente - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 {
     "name": "Crm Claim Related",
-    'version': '8.0.1.1.0',
-    'license': "AGPL-3",
-    'author': "AvanzOSC",
-    'website': "http://www.avanzosc.es",
-    'contributors': [
+    "version": "8.0.1.1.0",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/crm-addons",
+    "contributors": [
         "Ana Juaristi <anajuaristi@avanzosc.es>",
         "Alfredo de la Fuente <alfredodelafuente@avanzosc.es",
     ],
     "category": "Customer Relationship Management",
     "depends": [
-        'crm_claim',
+        "crm_claim",
     ],
     "data": [
-        'views/crm_claim_view.xml',
+        "views/crm_claim_view.xml",
     ],
     "installable": True,
 }
