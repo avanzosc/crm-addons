2d5285c063dd4a068ce5d4e523741ab0169f3356
Alfredo <alfredodelafuente@avanzosc.es>
2024-03-27 12:05:26 +0100

    [MIG] crm_claim_report: Migration to 16.0

---
+++ crm_claim_report/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Copyright Ainara Galdona - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Crm Claim Report",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "contributors": [
+        "Ainara Galdona <ainaragaldona@avanzosc.es",
+    ],
+    "category": "Customer Relationship Management",
+    "depends": [
+        "sales_team",
+        "crm_claim",
+        "crm_claim_code",
+        "crm_claim_type",
+        "crm_claim_ref_search"
+    ],
+    "data": [
+        "views/crm_claim_report_document.xml",
+        "views/crm_claim_report.xml",
+    ],
+    "installable": True,
+}
