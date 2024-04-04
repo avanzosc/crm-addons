2062356d8e42c09ea58b00dc2d20a6e58bbdb60e
Alfredo <alfredodelafuente@avanzosc.es>
2024-04-04 14:19:09 +0200

    [ADD] crm_claim_corrective_mgmtsystem: This module extends CRM Claims with corrective and preventive actions to handle them as appropriate.

---
+++ crm_claim_corrective_mgmtsystem/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2024 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "CRM Claim Corrective Mgmtsystem",
+    "version": "16.0.1.0.0",
+    "category": "Customer Relationship Management",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://www.avanzosc.es",
+    "depends": [
+        "crm_claim_corrective",
+        "mgmtsystem"
+    ],
+    "data": [
+        "views/crm_claim_corrective_view.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
