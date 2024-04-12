0d07107641d1d522a28bd18631d88f406d677450
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2024-04-12 11:14:17 +0200

    [ADD] crm_lead_marketing_info: This module displays the fields of marketing: "campaign", "channel", and "source", in the tree of leads, and opportunities. (#139)

---
+++ crm_lead_marketing_info/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# 2015 Serv. Tec. Avanzados - Pedro M. Baeza (http://www.serviciosbaeza.com)
+# 2015 AvanzOsc (http://www.avanzosc.es)
+# Copyright 2024 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "CRM Lead Marketing Info",
+    "version": "16.0.1.0.0",
+    "author": "OdooMRP team,"
+              "AvanzOSC,"
+              "Serv. Tecnol. Avanzados - Pedro M. Baeza",
+    "category": "Sales/CRM",
+    "license": "AGPL-3",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "crm",
+    ],
+    "data": [
+        "views/crm_lead_view.xml",
+    ],
+    "installable": True,
+}
