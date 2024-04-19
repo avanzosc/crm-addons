6633733f5d80a3099b9c0cf000b400020ed3c96d
Alfredo <alfredodelafuente@avanzosc.es>
2024-04-19 10:28:51 +0200

    [ADD] crm_lead_create_repair_payment: When creating a repair from opportunities, bring the shipping address of the opportunity to the repair.

---
+++ crm_lead_payment_repair/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2024 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Crm Lead Payment Repair",
+    "version": "14.0.1.2.0",
+    "category": "Inventory/Inventory",
+    "license": "AGPL-3",
+    "author": "Avanzosc",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "depends": [
+        "crm_lead_create_repair",
+        "crm_lead_payment",
+    ],
+    "data": [
+    ],
+    "installable": True,
+    "auto_install": True,
+}
