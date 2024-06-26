55ed4cf1377253f7a0477de3c050411b00792fec
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-06-26 10:39:00 +0200

    [IMP] crm_claim_deadline: pre-commit stuff

--- crm_claim_deadline/models/crm_claim.py
+++ crm_claim_deadline/models/crm_claim.py
@@ -1,6 +1,7 @@
 # (c) 2015 Alfredo de la Fuente - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 from dateutil.relativedelta import relativedelta
+
 from odoo import api, fields, models
 
 
