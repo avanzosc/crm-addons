1912ce41d775fa557f1dd04470ab8397888998f7
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-06-27 11:04:35 +0200

    [IMP] crm_campaign_configuration: pre-commit stuff

--- crm_campaign_configuration/models/res_partner.py
+++ crm_campaign_configuration/models/res_partner.py
@@ -1,14 +1,20 @@
 # Copyright 2021 Berezi - Iker - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import models, fields
+from odoo import fields, models
 
 
 class ResPartner(models.Model):
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
     campaign_id = fields.Many2one(
-        string='Campaign', comodel_name='utm.campaign')
+        string="Campaign",
+        comodel_name="utm.campaign",
+    )
     medium_id = fields.Many2one(
-        string='Medium', comodel_name='utm.medium')
+        string="Medium",
+        comodel_name="utm.medium",
+    )
     source_id = fields.Many2one(
-        string='Source', comodel_name='utm.source')
+        string="Source",
+        comodel_name="utm.source",
+    )
