f5a94dfb097fe0954a99eea4cd6c3b28cd5da82e
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-06-27 11:19:41 +0200

    [IMP] crm_event_lang_level: pre-commit stuff

--- crm_event_lang_level/models/crm_lead.py
+++ crm_event_lang_level/models/crm_lead.py
@@ -1,12 +1,16 @@
 # Copyright 2022 Alfredo de la Fuente - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import models, fields, api
+from odoo import fields, models
 
 
 class CrmLead(models.Model):
-    _inherit = 'crm.lead'
+    _inherit = "crm.lead"
 
     lang_id = fields.Many2one(
-        string='Language', comodel_name='hr.skill')
+        string="Language",
+        comodel_name="hr.skill",
+    )
     level_id = fields.Many2one(
-        string='Level', comodel_name='hr.skill.level')
+        string="Level",
+        comodel_name="hr.skill.level",
+    )
