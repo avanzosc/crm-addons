c38d9e9d554c7d9cb34bda4c8379185978847653
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-06-27 11:21:59 +0200

    [IMP] crm_claim_non_conformity_report: pre-commit stuff

--- crm_claim_non_conformity_report/models/res_users.py
+++ crm_claim_non_conformity_report/models/res_users.py
@@ -1,10 +1,9 @@
 # Copyright 2022 Berezi Amubieta - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import models, fields
+from odoo import fields, models
 
 
 class ResUsers(models.Model):
-    _inherit = 'res.users'
+    _inherit = "res.users"
 
-    sign = fields.Binary(
-        string='Signature')
+    sign = fields.Binary(string="Signature")
