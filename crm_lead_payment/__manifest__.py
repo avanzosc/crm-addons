a0b61eb4c9f8769f6018bcb712dd780e310cd425
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-06-27 11:11:19 +0200

    [IMP] crm_lead_payment: pre-commit stuff

--- crm_lead_payment/__manifest__.py
+++ crm_lead_payment/__manifest__.py
@@ -2,8 +2,9 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "CRM Lead Payment",
-    'version': '14.0.1.0.0',
+    "version": "14.0.1.0.0",
     "author": "Avanzosc",
+    "website": "https://github.com/avanzosc/crm-addons",
     "category": "Sales/CRM",
     "depends": [
         "sale_crm",
@@ -15,5 +16,5 @@
         "views/sale_order_view.xml",
     ],
     "license": "AGPL-3",
-    'installable': True,
+    "installable": True,
 }
