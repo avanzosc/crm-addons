3608d63c2c250f1ad63fcd36080e540509bf08f9
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-06-27 11:11:19 +0200

    [IMP] crm_lead_payment: pre-commit stuff

--- crm_lead_payment/models/sale_order.py
+++ crm_lead_payment/models/sale_order.py
@@ -8,18 +8,22 @@ class SaleOrder(models.Model):
 
     final_partner_id = fields.Many2one(
         string="Final User",
-        comodel_name="res.partner")
+        comodel_name="res.partner",
+    )
 
-    @api.onchange('opportunity_id')
+    @api.onchange("opportunity_id")
     def onchange_opportunity_id(self):
         if self.opportunity_id:
-            self.write({
-                "partner_invoice_id": (
-                    self.opportunity_id.partner_invoice_id.id),
-                "partner_shipping_id": (
-                    self.opportunity_id.partner_shipping_id.id),
-                "payment_term_id": (
-                    self.opportunity_id.property_payment_term_id.id),
-                "payment_mode_id": (
-                    self.opportunity_id.customer_payment_mode_id.id),
-                "final_partner_id": self.opportunity_id.final_partner_id.id})
+            self.write(
+                {
+                    "partner_invoice_id": (self.opportunity_id.partner_invoice_id.id),
+                    "partner_shipping_id": (self.opportunity_id.partner_shipping_id.id),
+                    "payment_term_id": (
+                        self.opportunity_id.property_payment_term_id.id
+                    ),
+                    "payment_mode_id": (
+                        self.opportunity_id.customer_payment_mode_id.id
+                    ),
+                    "final_partner_id": self.opportunity_id.final_partner_id.id,
+                }
+            )
