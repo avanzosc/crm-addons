ed6ab5b3eb8439e87f8ab63c86b4d5c7a4fc6c6d
Unai Beristain <unaiberis@gmail.com>
2024-09-03 10:55:44 +0200

    [ADD] crm_lead_lost_reason_add_info

---
+++ crm_lead_lost_reason_add_info/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "CRM Lead Lost Reason Additional Information",
+    "version": "14.0.1.0.0",
+    "summary": "Enhances CRM leads with detailed information on lost opportunities.",
+    "category": "Sales/CRM",
+    "author": "Avanzosc",
+    "website": "https://github.com/avanzosc/crm-addons",
+    "license": "AGPL-3",
+    "depends": [
+        "crm",
+    ],
+    "data": [
+        "views/crm_lead_views.xml",
+        "wizards/crm_lead_lost_reason_wizard_views.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
