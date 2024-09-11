4ca00caec4a5f30a45cdeae6fae51de8d141eaa7
Unai Beristain <unaiberis@gmail.com>
2024-09-11 10:41:51 +0200

    [ADD] crm_lead_stage_id_and_action_schedule_meeting: Add field and action

---
+++ crm_lead_stage_id_and_action_schedule_meeting/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "CRM Lead Stage and Action Schedule Meeting",
+    "version": "14.0.1.0.0",
+    "category": "Sales",
+    "summary": "Allows for different stages in Leads and Action Schedule Meeting.",
+    "author": "Avanzosc",
+    "website": "https://github.com/avanzosc/crm-addons",
+    "license": "LGPL-3",
+    "depends": ["crm"],
+    "data": [
+        "views/crm_lead_view.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
