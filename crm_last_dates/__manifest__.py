24196c19df4713555cdd9a23f1388b968e6c1d98
Unai Beristain <unaiberis@gmail.com>
2024-09-25 12:41:26 +0200

    [IMP] crm_last_dates

--- crm_last_dates/__manifest__.py
+++ crm_last_dates/__manifest__.py
@@ -5,7 +5,7 @@
     "summary": "Add last lead, meeting, and invoice dates to res.partner.",
     "website": "https://github.com/avanzosc/crm-addons",
     "license": "LGPL-3",
-    "depends": ["crm", "calendar.event", "account "],
+    "depends": ["crm", "calendar.event", "account"],
     "data": ["views/res_partner_view.xml"],
     "installable": True,
     "application": False,
