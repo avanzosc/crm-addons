126804de5290ff8c31e01f9929948d59e7f79105
Alfredo <alfredodelafuente@avanzosc.es>
2024-10-01 09:43:20 +0200

    [16.0][ADD] crm_claim_non_conformity: In crm claim type new field "Is Non Conformity?".

---
+++ crm_claim_non_conformity/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2024 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "CRM Claim Non Conformity",
+    "version": "16.0.1.0.0",
+    "category": "Customer Relationship Management",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/crm-addons",
+    "depends": [
+        "crm_claim_type_sequence",
+    ],
+    "data": [
+        "views/crm_claim_type_views.xml",
+    ],
+    "installable": True,
+}
