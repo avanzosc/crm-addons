1c8b685fd71bff2ede8d2179000d0127c9a8a427
Alfredo <alfredodelafuente@avanzosc.es>
2024-12-26 15:15:21 +0100

    [ADD] crm_claim_department: Department in claims.

---
+++ crm_claim_department/models/crm_claim.py
@@ -0,0 +1,12 @@
+# Copyright 2024 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import fields, models
+
+
+class CrmClaim(models.Model):
+    _inherit = "crm.claim"
+
+    department_id = fields.Many2one(
+        string="Department",
+        comodel_name="hr.department",
+    )
