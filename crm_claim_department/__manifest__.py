8a298047003b742cdaaa60b2ab058536731e701e
Alfredo <alfredodelafuente@avanzosc.es>
2024-12-26 15:15:21 +0100

    [ADD] crm_claim_department: Department in claims.

---
+++ crm_claim_department/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2024 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Crm Claim Department",
+    "version": "16.0.1.0.0",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/crm-addons",
+    "category": "Customer Relationship Management",
+    "depends": ["crm_claim", "hr"],
+    "data": [
+        "views/crm_claim_views.xml",
+    ],
+    "license": "AGPL-3",
+    "installable": True,
+}
