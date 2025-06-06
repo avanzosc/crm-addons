5c1b9216d1bba29e0735aef65e4c52cac94313e3
Ane <aneavanzosc@gmail.com>
2025-06-06 10:45:14 +0200

    [16.0][ADD] crm_claim_pcb: Adds a PCB field to CRM Claims

---
+++ crm_claim_pcb/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "CRM Claim PCB",
+    "version": "16.0.1.0.0",
+    "depends": ["crm_claim"],
+    "category": "CRM",
+    "summary": "Adds a PCB field to CRM Claims",
+    "data": [
+        "security/ir.model.access.csv",
+        "views/crm_claim_pcb_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "website": "https://github.com/avanzosc/crm-addons",
+    "author": "AvanzOSC",
+    "license": "AGPL-3",
+}
