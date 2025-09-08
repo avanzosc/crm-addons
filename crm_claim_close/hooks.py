edb34ad200f9383f5a7b0419e5957cf7916e00e8
Ane <aneavanzosc@gmail.com>
2025-09-08 14:04:00 +0200

    [18.0][MIG] crm_claim_close: Migration to 18.0

--- crm_claim_close/hooks.py
+++ crm_claim_close/hooks.py
@@ -3,22 +3,18 @@
 
 import logging
 
-from odoo import SUPERUSER_ID, api
-
 _logger = logging.getLogger(__name__)
 
 
-def post_init_hook(cr, registry):
+def post_init_hook(env):
     """
     Copy write date to closed date at claim taking into account if the stage is
     closed.
     """
-    with api.Environment.manage():
-        env = api.Environment(cr, SUPERUSER_ID, {})
-        m_claim = env["crm.claim"]
-        claims = m_claim.search([("stage_id.closed", "=", True)])
-        if claims:
-            _logger.info("Closing claims: %d contracts" % len(claims))
-        for claim in claims:
-            claim.date_closed = claim.write_date
-        _logger.info("Closing claims: Done")
+    m_claim = env["crm.claim"]
+    claims = m_claim.search([("stage_id.closed", "=", True)])
+    if claims:
+        _logger.info("Closing claims: %d contracts" % len(claims))
+    for claim in claims:
+        claim.date_closed = claim.write_date
+    _logger.info("Closing claims: Done")
