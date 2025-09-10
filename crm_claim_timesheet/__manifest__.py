b77ca5dd8cac4aafe012a564d1e5351c315e8352
Ane <aneavanzosc@gmail.com>
2025-09-10 08:45:59 +0200

    [18.0][MIG] crm_claim_timesheet: Migration to 18.0

--- crm_claim_timesheet/__manifest__.py
+++ crm_claim_timesheet/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Claim Timesheets",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Customer Relationship Management",
     "license": "AGPL-3",
     "author": "AvanzOSC",
