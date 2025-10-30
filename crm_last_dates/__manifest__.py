fa51a9824407f938ecb47a2ba4bddcbef2a7402b
Alfredo <alfredodelafuente@avanzosc.es>
2025-10-30 11:58:14 +0100

    [18.0][MIG] crm_last_dates: Migration to 18.0

--- crm_last_dates/__manifest__.py
+++ crm_last_dates/__manifest__.py
@@ -1,6 +1,8 @@
+# Copyright 2025 Unai Beristain - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "CRM Last Dates",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "author": "Avanzosc",
     "summary": "Add last lead, meeting, and invoice dates to res.partner.",
     "website": "https://github.com/avanzosc/crm-addons",
