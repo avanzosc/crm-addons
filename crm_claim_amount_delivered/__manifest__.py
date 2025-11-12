d3be1a511159d20253cf70272c34e7c57fd805af
Alfredo <alfredodelafuente@avanzosc.es>
2025-11-12 09:23:54 +0100

    [16.0][ADD] crm_claim_amount_delivered: In crm claim new field "Amount Delivered".

---
+++ crm_claim_amount_delivered/__manifest__.py
@@ -0,0 +1,17 @@
+# (c) 2015 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "CRM Claim Amount Delivered",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/crm-addons",
+    "category": "Customer Relationship Management",
+    "depends": [
+        "crm_claim",
+    ],
+    "data": [
+        "views/crm_claim_view.xml",
+    ],
+    "installable": True,
+}
