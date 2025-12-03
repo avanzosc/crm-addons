232dbf39253c2aa6af8339292c46893e324eb931
Alfredo <alfredodelafuente@avanzosc.es>
2025-12-03 07:36:14 +0100

    [16.0][ADD] crm_lead_visible_partner: In crm lead form view always show the "Partner" field.

---
+++ crm_lead_visible_partner/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2025 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "CRM Lead Visible Partner",
+    "version": "16.0.1.0.0",
+    "category": "Sales/CRM",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/crm-addons",
+    "depends": [
+        "crm",
+    ],
+    "data": [
+        "views/crm_lead_views.xml",
+    ],
+    "installable": True,
+}
