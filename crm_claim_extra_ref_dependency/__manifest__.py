3ca0587da782a008f53d9d2416a1ea78f7e27407
Alfredo <alfredodelafuente@avanzosc.es>
2026-01-28 13:27:52 +0100

    [16.0][ADD] crm_claim_extra_ref_dependency: Before deleting a purchase order, sales order, or picking, verify that they are not referenced in any claims.

---
+++ crm_claim_extra_ref_dependency/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2026 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "CRM Claim Extra Ref Dependency",
+    "version": "16.0.1.0.0",
+    "author": "AvanzOSC",
+    "category": "CRM Claim",
+    "license": "AGPL-3",
+    "website": "https://github.com/avanzosc/crm-addons",
+    "depends": ["crm_claim_extra_ref", "purchase", "sale", "stock"],
+    "data": [],
+    "installable": True,
+    "auto_install": True,
+}
