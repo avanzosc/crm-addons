f9c8cb0779ae1f2b8d48de9d2b8897dc89891eea
Alfredo <alfredodelafuente@avanzosc.es>
2026-01-29 07:20:13 +0100

    [16.0][ADD] crm_claim_search: In the claims "search" view, change the "Type" literal to "Category".

---
+++ crm_claim_search/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2026 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "CRM Claim Search",
+    "version": "16.0.1.0.0",
+    "category": "Customer Relationship Management",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/crm-addons",
+    "depends": ["crm_claim"],
+    "data": [
+        "views/crm_claim_views.xml",
+    ],
+    "installable": True,
+    "auto_install": True,
+}
