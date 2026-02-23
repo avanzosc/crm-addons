b31996f1911b1034f71661320ffd7cf8ab2747f5
Eñaut Alberdi <enautavanzosc@gmail.com>
2026-02-23 11:30:43 +0100

    [18.0][MIG] crm_claim_extra_ref: Migration to 18.0

---
+++ crm_claim_extra_ref/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright 2015 Pedro M. Baeza (http://www.serviciosbaeza.com)
+# Copyright 2024 Alfredo de la Fuente - AvanzOSC
+# Copyright 2026 Eñaut Alberdi - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "CRM Claim Extra Ref",
+    "version": "18.0.1.0.0",
+    "author": "OdooMRP team," "AvanzOSC," "Serv. Tecnol. Avanzados - Pedro M. Baeza",
+    "category": "CRM Claim",
+    "license": "AGPL-3",
+    "website": "https://github.com/avanzosc/crm-addons",
+    "contributors": [
+        "Pedro M. Baeza <pedro.baeza@serviciosbaeza.com",
+        "Ana Juaristi <anajuaristi@avanzosc.es>",
+        "Alfredo de la Fuente <alfredodelafuente@avanzosc.es>",
+        "Eñaut Alberdi <enautavanzosc@gmail.com>",
+    ],
+    "depends": ["crm_claim", "crm_claim_report"],
+    "data": [
+        "reports/crm_claim_report.xml",
+        "views/crm_claim_view.xml",
+    ],
+    "installable": True,
+}
