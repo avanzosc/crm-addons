cb50022fa8ca1baa290c6fd19790541de563cf24
Alfredo <alfredodelafuente@avanzosc.es>
2026-04-21 12:48:55 +0200

    [18.0][MIG] crm_claim_non_conformity: Migration to 18.0

--- crm_claim_non_conformity/__manifest__.py
+++ crm_claim_non_conformity/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "CRM Claim Non Conformity",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Customer Relationship Management",
     "license": "AGPL-3",
     "author": "AvanzOSC",
