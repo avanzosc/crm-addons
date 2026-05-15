a5891223a02bae8add39e68410b14d9854a2df9b
Eñaut Alberdi <enautavanzosc@gmail.com>
2026-05-15 13:32:37 +0200

    [18.0][ADD] crm_claim_extra_ref_report: ADD bridge to include extra refs in claim report without cross-dependencies

---
+++ crm_claim_extra_ref_report/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2026 Eñaut Alberdi - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "CRM Claim Extra Ref Report",
+    "version": "18.0.1.0.0",
+    "author": "AvanzOSC",
+    "category": "CRM Claim",
+    "license": "AGPL-3",
+    "website": "https://github.com/avanzosc/crm-addons",
+    "depends": ["crm_claim_extra_ref", "crm_claim_report"],
+    "data": [
+        "reports/crm_claim_report.xml",
+    ],
+    "installable": True,
+}
