19bc98ac380fb47dc1d8e433e443253ba489aa2c
luciaavanzosc <luciaavanzosc@gmail.com>
2026-09-22 13:15:25 +0200

    [18.0][FIX] crm_claim_ref_search: Do not crash on invalid claim references

--- crm_claim_ref_search/__manifest__.py
+++ crm_claim_ref_search/__manifest__.py
@@ -4,7 +4,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "CRM Claim Ref Search",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.0.1",
     "author": "OdooMRP team," "AvanzOSC," "Serv. Tecnol. Avanzados - Pedro M. Baeza",
     "category": "CRM Claim",
     "license": "AGPL-3",
